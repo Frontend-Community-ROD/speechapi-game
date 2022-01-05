b8d1960146f9b87095a91b0f17078cf7c2a5388a
patom1025 <patricia.montecchiarini@endava.com>
2022-01-05 19:03:16 -0300

    no anda

--- src/front/index.js
+++ src/front/index.js
@@ -1,7 +1,10 @@
 import GameBoard from './GameBoard';
 import Levels from './Levels';
 
-var pixiApp = new PIXI.Application({ width: 480, height: 480 });
+var pixiApp = new PIXI.Application({
+	width: 480,
+	height: 480
+});
 GameContainer.appendChild(pixiApp.view);
 
 let currentLevelNumber = 1;
@@ -9,9 +12,9 @@ let currentLevelConfig = Levels['' + currentLevelNumber];
 
 const gameBoardInstance = new GameBoard(currentLevelConfig);
 gameBoardInstance.onwin(() => {
-  currentLevelNumber++;
-  currentLevelConfig = Levels['' + currentLevelNumber];
-  gameBoardInstance.setLevel(currentLevelConfig);
+	currentLevelNumber++;
+	currentLevelConfig = Levels['' + currentLevelNumber];
+	gameBoardInstance.setLevel(currentLevelConfig);
 });
 
 pixiApp.stage.addChild(gameBoardInstance);
@@ -21,59 +24,74 @@ pixiApp.stage.addChild(gameBoardInstance);
 var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
 var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
 var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
-
-var controls = [ 'izquierda', 'derecha', 'arriba', 'abajo' ];
-var grammar = '#JSGF V1.0; grammar controls; public <control> = ' + controls.join(' | ') + ' ;';
-
 const speechControlsHandlers = {
-  'izquierda': () => gameBoardInstance.moveLeft(),
-  'derecha': () => gameBoardInstance.moveRight(),
-  'abajo': () => gameBoardInstance.moveDown(),
-  'arriba': () => gameBoardInstance.moveUp()
+	'izquierda': () => gameBoardInstance.moveLeft(),
+	'derecha': () => gameBoardInstance.moveRight(),
+	'abajo': () => gameBoardInstance.moveDown(),
+	'arriba': () => gameBoardInstance.moveUp()
 }
 
 let recognitionStarted = false;
+let movedPiece = false;
+const confidenceThreshold = .90;
+
+var controls = Object.keys(speechControlsHandlers);
+//var grammar = '#JSGF V1.0; grammar controls; public <control> = ' + controls.join(' | ') + ' ;';
 
 var recognition = new SpeechRecognition();
 var speechRecognitionList = new SpeechGrammarList();
 
-speechRecognitionList.addFromString(grammar, 1);
+//speechRecognitionList.addFromString(grammar, 1);
 
 recognition.grammars = speechRecognitionList;
-recognition.continuous = false;
-recognition.lang = 'es';
+recognition.continuous = true;
+recognition.lang = 'es-ES';
 recognition.interimResults = false;
 recognition.maxAlternatives = 1;
 
-document.body.onclick = function() {
+document.body.onclick = function () {
 
-  if (recognitionStarted) {
-    console.log('Already capturing...');
-    return;
-  }
+	if (recognitionStarted) {
+		console.log('Already capturing...');
+		return;
+	} else {
 
-  recognition.start();
-  recognitionStarted = true;
-  console.log('Ready to receive a commands.');
+		recognition.start();
+		recognitionStarted = true;
+		console.log('Ready to receive a commands.');
+	}
+
 }
 
-recognition.onstart = function() {
-  console.log('Capturing started...');
+recognition.onstart = function () {
+	console.log('Capturing started...');
+	movedPiece = false
 }
 
-recognition.onresult = function(event) {
-  const speech = event.results[0][0].transcript;
-  console.log("results", event.results);
-  console.log('Confidence: ' + event.results[0][0].confidence);
-
-  const speechControls = speech.split(' ').filter(said => controls.includes(said));
-  console.log('speech', speechControls);
-  speechControls.forEach(cont => speechControlsHandlers[cont]());
+recognition.onresult = function (event) {
+	const {
+		transcript,
+		confidence
+	} = event.results[0][0]
+	console.log(transcript)
+	if (confidence < confidenceThreshold) {
+		recognition.end()
+		return
+	};
+	const actionControl = transcript.split(' ').find(said => [...controls].includes(said));
+	if (!!actionControl) {
+		const action = speechControlsHandlers[actionControl]
+		if (action) {
+			action()
+			movedPiece = true;
+		}
+	}
 }
 
-recognition.onend = function() {
-  console.log('Capturing ended...');
-  recognition.start();
+recognition.onend = function (event) {
+
+	if (movedPiece) {
+		console.log('Capturing ended...');
+		recognition.start();
+	}
 }
-
-
